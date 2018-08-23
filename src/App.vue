9a088fb4ea757d16ca5d5e4e964274311ac326fe
jie <j@yahoo.com>
2018-08-23 17:31:35 +0800

    axios test

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,34 @@
 <template>
-  <div style="width: 50%;">
-    <todo-header @addTodo="addTodo"></todo-header> <!--改用自訂義觸發-->
-    <todo-list :todos="todos" :delTodo="delTodo"></todo-list>
-    <todo-footer :todos="todos" :delCompleteTodos="delCompleteTodos" :selectAllTodos="selectAllTodos"/>
+  <div id="app">
+    <div v-if="!repoUrl">loading..</div>
+    <div v-else>most star repo is <a :herf="repoUrl">{{repoName}}</a></div>
   </div>
 </template>
 
 <script>
-  import TodoHeader from './components/TodoHeader.vue'
-  import TodoList from './components/TodoList.vue'
-  import TodoFooter from './components/TodoFooter.vue'
-  import storageUtil from './util/storageUtil'
+  import axios from 'axios'
   export default {
     data () {
       return {
-        /* 取得localStorage的值 並且轉成json */
-        todos: storageUtil.readTodos()
+        repoUrl: '',
+        repoName: ''
       }
     },
-    methods: {
-      addTodo (todo) {
-        this.todos.unshift(todo)
-      },
-      delTodo (index) {
-        this.todos.splice(index, 1)
-      },
-      delCompleteTodos () {
-        /* '過濾' 出沒有完成的 */
-        this.todos = this.todos.filter(todo => !todo.complete)
-      },
-      selectAllTodos (isCheck) {
-        /* 遍歷所有的todo 把complete值改為isCheck */
-        this.todos.forEach(todo => todo.complete = isCheck)
-      }
-    },
-    watch: {
-      todos: {
-        deep: true, /* 深度監視todos有變動就存檔 */
-//        handler: function (value) {
-//          storageUtil.saveTodos(value)
-//        } 改寫如下
-        handler: storageUtil.saveTodos
-      }
-    },
-    components: {
-      TodoHeader,
-      TodoList,
-      TodoFooter
+    mounted () {
+      const url = `https://api.github.com/search/repositories?q=vu&sort=stars`
+      axios.get(url).then(
+        response => { // --成功
+          const result = response.data
+          const mostRepo = result.items[0]
+          this.repoUrl = mostRepo.html_url
+          this.repoName = mostRepo.name
+        }).catch(e => {
+          console.log(e.message)
+          alert('失敗了!!')
+      })
     }
   }
 </script>
 
 <style>
-
 </style>
