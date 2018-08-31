bc54d201e0264653572bfdffce7f58bdea224f87
jie <j@yahoo.com>
2018-08-31 10:16:04 +0800

    router

--- src/views/MessageDetail.vue
+++ src/views/MessageDetail.vue
@@ -1,13 +1,25 @@
 <template>
   <ul>
     <p>ID: {{$route.params.id}}</p>
-    <p>ID: {{$route.params.id}}</p>
-    <li>title:AA</li>
-    <li>content:aaaaa</li>
+    <li>title:{{MessageDetail.title}}</li>
+    <li>content:{{MessageDetail.content}}</li>
   </ul>
 </template>
 
 <script>
+
+  export default {
+    data () {
+      return {
+        MessageDetail: {
+          id: '',
+          title: '',
+          content: ''
+        }
+      }
+    },
+    mounted () {
+setTimeout(() => { // 模擬資料庫取出資料
   const allMessageDetail = [
     {
       id: 1,
@@ -25,18 +37,14 @@
       content: 'ccc.....'
     }
   ]
-  export default {
-    data () {
-      return {
-        MessageDetail: {
-          id: '',
-          title: '',
-          content: ''
-        }
-      }
+  this.allMessageDetail = allMessageDetail
+}, 500)
     },
-    mounted () {
-
+    watch: { // 因為路由位置沒變在同個頁面變換而已，所以不能寫在mounted，要監視$route變化就啟動
+      $route: function (value) {
+        const id = value.params.id * 1 // 這是畫面上點選的ID  強制轉成數字
+        this.MessageDetail = this.allMessageDetail.find(detail => detail.id === id) // 查詢出跟畫面ID一樣的物件
+      }
     }
   }
 </script>
