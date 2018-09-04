8c4cd059ba95a59d5d0d09b3b54978ddaf540fb5
jie <j@yahoo.com>
2018-09-04 17:49:34 +0800

    45 router

--- src/views/Message.vue
+++ src/views/Message.vue
@@ -2,9 +2,16 @@
   <div>
     <ul>
       <li v-for="msg in messages" :key="msg.id">
+        <!-- a標籤 -->
         <router-link :to="`/home/message/detail/${msg.id}`">{{msg.title}}</router-link>
+        <!-- @click方式ROUTER -->
+        <!-- push相當於 A連結 返回還是可以在當前介面 -->
+        <button @click="pushShow(msg.id)">push 查看</button>
+        <!-- replace 是給予新的路由替換當前路由 返回是在上一個介面 -->
+        <button @click="replaceShow(msg.id)">replace 查看</button>
       </li>
     </ul>
+    <button @click="$router.back()">上一步</button>
     <hr>
     <router-view></router-view>
   </div>
@@ -36,10 +43,17 @@
         ]
         this.messages = messages
       }, 1000)
+    },
+    methods: {
+      pushShow (id) {
+        this.$router.push(`/home/message/detail/${id}`)
+      },
+      replaceShow (id) {
+        this.$router.replace(`/home/message/detail/${id}`)
+      }
     }
   }
 </script>
-
 <style>
 
 </style>
